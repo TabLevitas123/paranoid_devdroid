66d9b342ce97d5b0cbeb2979007d31ec0a8d3407
Tab Levitas <126153560+TabLevitas123@users.noreply.github.com>
2024-10-23 03:43:33 -0400

    Add example.py script

---
+++ example.py
@@ -0,0 +1,7 @@
+
+# Example Python script to demonstrate GitHub integration
+def example_function():
+    return "This is a new example function!"
+
+if __name__ == "__main__":
+    print(example_function())
